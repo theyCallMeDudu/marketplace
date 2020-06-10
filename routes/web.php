6fc3705778acd49ac44c9fa1947371d3a29c0ceb
Eduardo Coelho <52638570+theyCallMeDudu@users.noreply.github.com>
2020-06-10 16:46:12 -0300

    Criação dos models store e products

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,37 @@ Route::get('/', function () {
 
     return view('welcome', compact('fruta'));
 });
+
+Route::get('/model', function(){
+    //$products = \App\Product::all();
+
+    // Active Record
+//    $user = new \App\User();
+//    $user->name = 'Usuário Teste';
+//    $user->email = 'email@teste.com';
+//    $user->password = bcrypt('12345678');
+//    $user->save();
+
+    // return \App\User::all(); -> ** retorna todos os usuários.
+    // return \App\User::find(3); -> ** retorna um usuário com base no id passado.
+    // return \App\User::where('name', 'Fulano de Tal')->get(); -> ** retorna (select * from users where name = 'Fulano
+    // de Tal')
+    // return \App\User::paginate(10); -> ** paginar dados com Laravel
+
+    // Mass Assignment - Atribuição em massa
+//    $user = \App\User::create([
+//        'name' => 'Fulaninho de Tal',
+//        'email' => 'fulaninho@detal.com',
+//        'password' => bcrypt('12345678')
+//    ]);
+//    dd($user);
+
+    // Mass Update - Atualização em massa
+        $user = \App\User::find(43);
+        $user->update([
+            'name' => 'Atualizando com MASS UPDATE'
+        ]); // retorna true ou false
+        dd($user);
+
+    return \App\User::all();
+});
